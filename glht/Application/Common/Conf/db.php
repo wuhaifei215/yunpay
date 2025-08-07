010f4d4c4b251b6ed944ae54fa20e87a59ffc36e
wuhaifei215 <wuhaifei215@gmail.com>
2025-08-07 14:48:40 +0800

    Update db.php

--- glht/Application/Common/Conf/db.php
+++ glht/Application/Common/Conf/db.php
@@ -2,11 +2,11 @@
 // 数据库配置
 return array(
     'DB_TYPE'   => 'mysql',       // 数据库类型
-    'DB_HOST'   => '172.31.24.109,172.31.39.165',       // 服务器地址
-    'DB_NAME'   => 'yunphp',       // 数据库名
-    'DB_USER'   => 'yunphp',       // 用户名
-    'DB_PWD'    => 'PKawEW5cGTFJ6HXD',        // 密码
-    'DB_PORT'   => '20951',       // 端口
+    'DB_HOST'   => 'yun.cz8smss2imci.sa-east-1.rds.amazonaws.com,yuninstance.cz8smss2imci.sa-east-1.rds.amazonaws.com',       // 服务器地址
+    'DB_NAME'   => 'innodb',       // 数据库名
+    'DB_USER'   => 'yunAdmin',       // 用户名
+    'DB_PWD'    => 'XiaomiyunBRL',        // 密码
+    'DB_PORT'   => '3306',       // 端口
     'DB_PREFIX' => 'pay_',     // 数据库表前缀
     
     'DB_DEPLOY_TYPE'        =>  1, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
